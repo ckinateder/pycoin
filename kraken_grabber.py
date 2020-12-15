7958fc52929a2cc5563a9cf46f3ce0be39f4ff45
Calvin Kinateder <calvinkinateder1@yahoo.com>
2020-12-15 10:43:00 -0500

    update kraken_grabber

--- kraken_grabber.py
+++ kraken_grabber.py
@@ -1,14 +1,17 @@
-import kraken, json, pprint, pandas, time
+import kraken, json, pprint, pandas, time, os
+
+filename = 'overnight_data.csv'
 
 kt = kraken.KrakenTrader()
 reply = json.loads(kt.main(['Ticker', 'pair=xbtusd']))['result']['XXBTZUSD']
 
-header = list()
-total = list()
-
-for i in reply.items():
-    header.append(i[0])
-total.append(header)
+if not os.path.isfile(filename): # only add header if file doesnt exist
+    header = list()
+    header.append('unix')
+    for i in reply.items():
+        header.append(i[0])
+    #total.append(header)
+    pandas.DataFrame([header]).to_csv(filename, mode='a', header=False,index=False)
 
 while True:
     reply = json.loads(kt.main(['Ticker', 'pair=xbtusd']))['result']['XXBTZUSD']
@@ -17,6 +20,5 @@ while True:
     for i in reply.values():
         dropped.append(i[0])
     print(dropped)
-    total.append(dropped)
-    pandas.DataFrame(total).to_csv("overnight_data.csv", mode='a', header=False)
+    pandas.DataFrame([dropped]).to_csv(filename, mode='a', header=False,index=False)
     time.sleep(10)
