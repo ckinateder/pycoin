8af2aba7794102362ede44a3f7dd719c7b963b2a
Calvin Kinateder <calvinkinateder1@yahoo.com>
2020-12-29 12:10:15 -0500

    rewrite kraken grabber tp work with multiple args

--- kraken_grabber.py
+++ kraken_grabber.py
@@ -7,7 +7,9 @@ kt = kraken.KrakenTrader()
 
 while True:
     try:
-        kt.saveTickerPair(sys.argv[1:])
+        for i in range(1, len(sys.argv), 2):
+            pair = sys.argv[i:i+2]
+            kt.saveTickerPair(pair)
         time.sleep(10)
     except Exception as e:
         print('* Call failed... trying again in 2\n* Message: {}'.format(e))
--- multi-grabber.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python3 kraken_grabber.py eth usd &
-python3 kraken_grabber.py ltc usd &
-python3 kraken_grabber.py xbt usd &
-python3 kraken_grabber.py xrp usd &
-python3 kraken_grabber.py rep usd && fg
